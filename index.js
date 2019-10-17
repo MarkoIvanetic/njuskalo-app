448f3fb9a50fb5441a389032fd6df9cd93224d90
marko.ivanetic <marko.ivanetic@cloudsensesolutions.com>
2019-10-17 17:12:40 +0200

    Test 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hey, I\'m a Node.js app TEST 2!')
+    res.send('Hey, I\'m a Node.js app TEST 3!')
 })
 
 app.listen(3000, () => {
